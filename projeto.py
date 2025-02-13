e8dcadd97f97d7e9e76ec19b51e5e2983df7a03e
Alan-campos17 <149601081+Alan-campos17@users.noreply.github.com>
2025-02-13 18:08:42 +0000

    22

---
+++ projeto.py
@@ -0,0 +1,25 @@
+print("********************************")
+print("bem vindo ao jogo de adivinhação")
+print("********************************")
+
+numero_secreto = 42
+total_de_tentativas = 3
+rodada = 1
+
+while (rodada <= total_de_tentativas):
+    print(f"Tentativa {rodada} de {total_de_tentativa}")
+
+    chute_str = input("digite o seu numero:")
+    print("Você digitou o número:", chute_str)
+    chute = int(chute_str)
+
+    acertou = chute = => numero_acertou
+    maior = chute = => numero_maior
+    secreto = chute = => numero_secreto
+
+    if(acertou)
+    print("Parabens, você acertou!")
+    else:
+        if(maior):
+            print("o seu chute foi maior que o número secreto")
+            
