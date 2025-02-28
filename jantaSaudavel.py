618899d129247de1042d88e00180b838a5c27707
Alan-campos17 <149601081+Alan-campos17@users.noreply.github.com>
2025-02-28 19:01:29 +0000

    33

--- jantaSaudavel.py
+++ jantaSaudavel.py
@@ -1,22 +1,17 @@
-jantaNormal = ["salsicha","X-bacon","Miojo","Coca-cola","Pizza-Doce"]
+# Lista inicial de jantaNormal
+jantaNormal = ["salsicha", "X-bacon", "Miojo", "Coca-cola", "Pizza-Doce"]
 
+# Adicionando novos itens à lista
 jantaNormal.append("torresmo")
-jantaNormal.append()
-jantaNormal.append()
-jantaNormal.append()
-jantaNormal.append()
-jantaNormal.append()
-jantaNormal.append()
-jantaNormal.append()
-jantaNormal.pop()
-jantaNormal.pop()
-jantaNormal.pop()
-jantaNormal.pop()
-jantaNormal.pop()
-jantaNormal.pop()
-jantaNormal.pop()
-jantaNormal.pop()
-jantaNormal.pop()
-jantaNormal.pop()
+jantaNormal.append("hamburguer")
+jantaNormal.append("batata frita")
+jantaNormal.append("salada")
+jantaNormal.append("suco")
 
+# Removendo os últimos 5 itens adicionados
+for _ in range(5):
+    if jantaNormal:  # Verifica se a lista não está vazia
+        jantaNormal.pop()
+
+# Imprimindo a lista final
 print(jantaNormal)
