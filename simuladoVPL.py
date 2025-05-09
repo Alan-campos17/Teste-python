7f4196ccebe4a226a04758aee8dbe70db51ae008
Alan-campos17 <149601081+Alan-campos17@users.noreply.github.com>
2025-05-09 19:13:13 +0000

    66

--- simuladoVPL.py
+++ simuladoVPL.py
@@ -6,11 +6,20 @@ contador = 1
 qnFluxosCaixa = int(input("Digite a quantidade de fluxos de caixa:"))
 valorPresenteLiquido = 0
 while contador <= qnFluxosCaixa:
-    faturamento = float(input("Digite o faturamento do ano {contador}:"))
+    faturamento = float(input(f"Digite o faturamento do ano {contador}:"))
     valorPresenteLiquido += faturamento / (1 + taxaPorcentagem) ** contador
     contador += 1
 
-    if(valorPresenteLiquido - investimentoInicial) > 0:
-        print( " O VPL é positivo, o investimento é viavel")
-    else:
-        print("O VPL é negativo, o investimento não é viavel")
+if(valorPresenteLiquido - investimentoInicial) > 0:
+    print( " O VPL é positivo, o investimento é viavel")
+else:
+    print("O VPL é negativo, o investimento não é viavel")
+
+   
+
+Payback = valorPresenteLiquido / investimentoInicial
+if Payback > 1:
+    print("empresa já se pagou")
+    
+else:
+    print("A empresa ainda não se pagou")
